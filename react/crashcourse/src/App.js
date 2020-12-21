1cbc19eacd0461690fd9dbebd3999b13c4f5e12c
Ivan Burda <burda.ivan@gmail.com>
2020-12-21 22:59:47 +0100

    progressing with the react crashcourse

--- react/crashcourse/src/App.js
+++ react/crashcourse/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos.js";
 import "./App.css";
 
@@ -35,10 +36,22 @@ class App extends Component {
     });
   };
 
+  //Delete a todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Header />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
