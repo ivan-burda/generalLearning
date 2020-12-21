0885b45cf2f1fcd7cff4e70aa049ec0ec93b3f46
Ivan Burda <burda.ivan@gmail.com>
2020-12-22 00:35:21 +0100

    implementing dynamic ids

--- react/crashcourse/src/App.js
+++ react/crashcourse/src/App.js
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos.js";
 import AddTodo from "./components/AddTodo.js";
-import uuid from "uuid";
 import "./App.css";
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: uuid.v4(),
+        id: 1,
         title: "Take out the trash",
         completed: false,
       },
       {
-        id: uuid.v4(),
+        id: 2,
         title: "Dinner with wife",
         completed: false,
       },
       {
-        id: uuid.v4(),
+        id: 3,
         title: "Meeting with boss",
         completed: false,
       },
@@ -45,10 +44,33 @@ class App extends Component {
     });
   };
 
+  //Get next id
+  getNextId() {
+    let existingIds = this.state.todos.map((todo) => todo.id);
+    existingIds.sort((a, b) => a - b);
+
+    if (existingIds[existingIds.length - 1] === undefined) {
+      console.log(existingIds);
+      return 1;
+    } else {
+      console.log(existingIds);
+      let next;
+      console.log(next);
+      for (let i = 1; i <= existingIds[existingIds.length - 1]; i++) {
+        if (existingIds.indexOf(i) === -1) {
+          console.log(i);
+          next = i;
+        }
+      }
+      //return existingIds[existingIds.length - 1] + 1;
+      return next;
+    }
+  }
+
   //Add a todo
   addTodo = (title) => {
     const newTodo = {
-      id: uuid.v4(),
+      id: this.getNextId(),
       title,
       completed: false,
     };
