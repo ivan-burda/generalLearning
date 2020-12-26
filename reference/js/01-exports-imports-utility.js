25e43d2aa0f232a1f495a3de8cb70cf7591ccbdf
Ivan Burda <burda.ivan@gmail.com>
2020-12-26 23:58:01 +0100

    references exp/imp

--- reference/js/01-exports-imports-utility.js
+++ reference/js/01-exports-imports-utility.js
@@ -2,4 +2,8 @@ export const greet = (name) => {
   return `Hello ${name}!`;
 };
 
+export const greet2 = (name) => {
+  return `Howdy ${name}!`;
+};
+
 export const politeRemark = "Nice to meet you.";
--- reference/js/01-exports-imports.js
+++ reference/js/01-exports-imports.js
@@ -1,4 +1,8 @@
 import person from "./01-exports-imports-person.js";
-import { greet, politeRemark } from "./01-exports-imports-utility.js";
+//import osoba from "./01-exports-imports-person.js"; -> this would be alright too; The name does not matter for files with default export
+
+import { greet, politeRemark } from "./01-exports-imports-utility.js"; //The exact name matter for files without exact default export
+import { greet2 as pozdrav2 } from "./01-exports-imports-utility.js"; //Even though the exact name must be used for named export, it is possible to set a new name for further referencing
 
 console.log(greet(person.name) + politeRemark);
+console.log(pozdrav2(person.name) + politeRemark);
