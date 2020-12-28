b056368997b915fc558b21e22c7c28b767c85d5e
Ivan Burda <burda.ivan@gmail.com>
2020-12-28 23:53:41 +0100

    learning about react props

--- react/react-complete/react-complete-guide/src/App.js
+++ react/react-complete/react-complete-guide/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World from React!</h1>
+        <Person name="Ivan" age="34"></Person>
+        <Person name="Stepan" age="32"></Person>
+        <Person name="Zdenek" age="28"></Person>
       </div>
     );
   }
