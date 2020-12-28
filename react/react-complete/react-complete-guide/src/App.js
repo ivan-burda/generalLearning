30a3f53ff7beabc01f02f10a4efd2202c309b4d6
Ivan Burda <burda.ivan@gmail.com>
2020-12-29 00:33:31 +0100

    react state

--- react/react-complete/react-complete-guide/src/App.js
+++ react/react-complete/react-complete-guide/src/App.js
@@ -3,13 +3,37 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Ivan", age: 34 },
+      { name: "Stepan", age: 32 },
+      { name: "Zdenek", age: 28 },
+    ], //can be given any name, does not have to be an array, can contain anything
+  };
+
+  switchNameHandler = () => {
+    console.log("Was clicked");
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello World from React!</h1>
-        <Person name="Ivan" age="34"></Person>
-        <Person name="Stepan" age="32"></Person>
-        <Person name="Zdenek" age="28"></Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        ></Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobby: Crafting
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        ></Person>
       </div>
     );
   }
