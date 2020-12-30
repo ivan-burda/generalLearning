533037a92b419561af5e3961469ab26a5660895e
Ivan Burda <burda.ivan@gmail.com>
2020-12-30 21:34:29 +0100

    commited all learning-related changes

---
+++ reference/react/02-stateless-vs-stateful-components.js
@@ -0,0 +1,12 @@
+//Stateful compnent
+/*
+aka: smart components, container components
+-use these only if neccesary, so there should not be too many of them
+-having too many state compnents can easily lead to spaghetti code
+*/
+
+//Stateless component
+/*
+aka: dumb components, presentational components
+-preferably use these as much as possible
+*/
