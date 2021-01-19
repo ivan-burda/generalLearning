d6e3d8d869b3fc0cbd43bd40d9f031312b13e40c
ivanburda1986 <burda.ivan@gmail.com>
2021-01-19 23:55:28 +0100

    burger builder

--- react/react-complete/section8-burger-builder/src/components/Burger/Burger.js
+++ react/react-complete/section8-burger-builder/src/components/Burger/Burger.js
@@ -3,6 +3,12 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) =>{
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientsKey => {
+      return [...Array(props.ingredients[ingredientsKey])].map((_, i)=>{
+        <BurgerIngredient key={ingredientsKey + 1} type={ingredientsKey}/>
+      })
+    });
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
