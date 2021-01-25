27317d6daef547a0a5541266a526a12200fff998
ivanburda1986 <burda.ivan@gmail.com>
2021-01-26 00:51:23 +0100

    added burger menu simples styling

--- react/react-complete/section8-burger-builder/src/components/Navigation/Toolbar/SideDrawerToggler/SideDrawerToggler.js
+++ react/react-complete/section8-burger-builder/src/components/Navigation/Toolbar/SideDrawerToggler/SideDrawerToggler.js
@@ -3,7 +3,9 @@ import classes from './SideDrawerToggler.css';
 
 const sideDrawerToggler = (props) =>(
   <div className={classes.SideDrawerToggler} onClick={props.sideDrawerOpen}>
-    <p>Sidemenu</p>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 );
 
