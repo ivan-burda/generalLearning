a3977b4bbb1aa4e65294d8212dcf84fcf7df6b5b
ivanburda1986 <burda.ivan@gmail.com>
2021-01-29 23:57:35 +0100

    interceptors

--- react/react-complete/section9-http/src/containers/Blog/Blog.js
+++ react/react-complete/section9-http/src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false,
     }
 
 
@@ -27,6 +28,9 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts});
                 //console.log(response);
             })
+            .catch(error=>{
+                this.setState({error: true});
+            })
     }
 
     postSelectedHandler(id){
@@ -34,14 +38,20 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post=>{
-            return <Post 
-            key={post.id} 
-            title={post.title} 
-            author={post.author}
-            clicked={()=> this.postSelectedHandler(post.id)}
-            />;
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+
+        if (!this.state.error){
+            posts = this.state.posts.map(post=>{
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={()=> this.postSelectedHandler(post.id)}
+                />;
+            })
+        }
+
+       
 
 
         return (
