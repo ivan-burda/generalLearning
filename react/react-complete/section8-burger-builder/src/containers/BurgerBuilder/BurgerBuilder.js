412f7ed6ae7cd964fa1e0344236bbb9f254ae263
ivanburda1986 <burda.ivan@gmail.com>
2021-01-30 03:21:59 +0100

    POST to firebase

--- react/react-complete/section8-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react/react-complete/section8-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -74,7 +76,29 @@ class BurgerBuilder extends Component{
   }
 
   purchaseContinueHandler = ()=>{
-    alert('You continue');
+    //alert('You continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, //in a realapp the price should be recalculated on the server to avoid tamperring
+      customer:{
+        name: 'Ivan Burda',
+        address: {
+        street: 'Winerstrasse 27',
+        zipCode: '1010',
+        country: 'Austria',
+        city: 'Wien',
+        },
+        email: 'test@gmail.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response =>{
+        console.log(response);
+      })
+      .catch(error=>{
+        console.log(error);
+      })
   };
 
   render(){
