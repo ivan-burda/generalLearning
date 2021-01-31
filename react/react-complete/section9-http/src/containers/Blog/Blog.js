e5b4bd0a053bbb1a92d24c4488f49e35fd2068b9
ivanburda1986 <burda.ivan@gmail.com>
2021-01-31 19:57:57 +0100

    routing

--- react/react-complete/section9-http/src/containers/Blog/Blog.js
+++ react/react-complete/section9-http/src/containers/Blog/Blog.js
@@ -1,72 +1,50 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 //--> to use my axios instance instead of the common axios from the package:
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import {Route, NavLink} from 'react-router-dom';
+
+
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-    }
-
-
-
-    componentDidMount(){
-        axios.get('/posts')
-            .then(response=>{
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post=>{
-                    return{
-                        ...post,
-                        author: 'Ivan'
-                    }
-                })
-                this.setState({posts: updatedPosts});
-                //console.log(response);
-            })
-            .catch(error=>{
-                this.setState({error: true});
-            })
-    }
-
-    postSelectedHandler(id){
-        this.setState({selectedPostId:id});
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-
-        if (!this.state.error){
-            posts = this.state.posts.map(post=>{
-                return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={()=> this.postSelectedHandler(post.id)}
-                />;
-            })
-        }
-
-       
-
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className={"Blog"}>
+                <header>
+                    <nav>
+                        <ul>
+
+                            <li><NavLink 
+                            to="/" 
+                            exact
+                            activeClassName="my-active"
+                            activeStyle={{
+                                color: 'green',
+                                textDecoration: 'underline',
+                            }}>Home</NavLink></li>
+                            {/* The value of to can be an object within JSX code. In this object we setup the NavLink attributes. It is possible to sertup also a #fragment and query parameters, however, in the example below it is just for a demonstration */}
+                            
+                            <li><NavLink to={{
+                                //An absolute path
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* It is fully OK to: use multiple routes even for the same path; nest routes */}
+                {/* <Route path="/" exact render={()=><h1>Home</h1>}/>
+                <Route path="/"  render={()=><h1>Home2</h1>}/> */}
+
+                {/* Loading components instead of rendering some JSX right at the spot */}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" exact component={NewPost}/>
+
             </div>
         );
     }
