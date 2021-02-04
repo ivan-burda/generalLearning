9f3cc990d9693c3bf739f9bef70448e16b042bdd
ivanburda1986 <burda.ivan@gmail.com>
2021-02-04 22:39:04 +0100

    basename

--- react/react-complete/section9-http/src/App.js
+++ react/react-complete/section9-http/src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //basename should be used if my react app is on the server served from a subdirectory. In that case the value of basename should be the subdir name
+      //<BrowserRouter basename="/my-app-subdir-name">
       <BrowserRouter>
       <div className="App">
         <Blog />
