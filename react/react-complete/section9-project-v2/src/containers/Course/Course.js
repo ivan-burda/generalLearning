c802425313cb758d61fa2db42fce4c5a9df8bb64
ivanburda1986 <burda.ivan@gmail.com>
2021-02-06 20:14:41 +0100

    removed clg

--- react/react-complete/section9-project-v2/src/containers/Course/Course.js
+++ react/react-complete/section9-project-v2/src/containers/Course/Course.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Course extends Component {
     
     render () {
+        //console.log(this.props.match.params);
         return (
             <div>
                 <h1>{this.props.title}</h1>
