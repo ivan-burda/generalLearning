7671eb20fb42c75c75c6cd5aa8d61d1bc56ced63
ivanburda1986 <burda.ivan@gmail.com>
2021-02-11 23:59:00 +0100

    changing the submitt button state based on the form overal validity

--- react/react-complete/section8-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ react/react-complete/section8-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -108,6 +108,7 @@ class ContactData extends Component{
           value: 'fastest'
         }
     },
+    formIsValid: false,
     loading: false,
   }
 
@@ -163,8 +164,14 @@ class ContactData extends Component{
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
-    this.setState({orderForm: updatedOrderForm});
+
+    //Setting the state of the overal form validity
+    let formIsValid = true;
+    for (let inputIdentifiers in updatedOrderForm){
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
   }
 
 render(){
@@ -192,7 +199,7 @@ render(){
       changed={(event)=>this.inputChangedHandler(event, formElement.id)}
       />
     ))}
-    <Button btnType="Success">ORDER</Button>
+    <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
   </form>
   );
 
