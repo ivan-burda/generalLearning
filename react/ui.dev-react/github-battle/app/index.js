8899225e703d234e8a6ef72ede541362b7a6ebda
ivanburda1986 <burda.ivan@gmail.com>
2021-03-02 23:42:16 +0100

    setup the react app a react app manually with babel and webpack

---
+++ react/ui.dev-react/github-battle/app/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//Component
+//-State
+//-Lifecycle
+//-UI
+
+class App extends React.Component{
+
+  render(){
+
+    return(
+      <div>
+        Hello Ivan!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+//React element
+//Where to render the element to
+  
+);
