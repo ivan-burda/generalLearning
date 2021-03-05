35b787f0bc0be6b04bafe170417d56decb7421bc
ivanburda1986 <burda.ivan@gmail.com>
2021-03-05 23:55:38 +0100

    abstracted the navigation to a separate componenet

--- react/ui.dev-react/github-battle/app/components/Popular.js
+++ react/ui.dev-react/github-battle/app/components/Popular.js
@@ -1,21 +1,52 @@
 import React from 'react';
 
+function LanguagesNav ({selected, onUpdateLanguage}){
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return(
+  <ul className="flex-center">
+          {languages.map((language)=>{
+            return(
+              <li key={language}>
+                <button 
+                className="btn-clear nav-link" 
+                style={language === selected ? {color: 'rgb(187,46,31)'} : null}
+                onClick={()=>onUpdateLanguage(language)}>
+                  {language}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+  );
+}
+
 class Popular extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    }
+
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage){
+    this.setState({selectedLanguage: selectedLanguage});
+  }
+
+
   render(){
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const {selectedLanguage} = this.state;
 
     return(
-      <ul className="flex-center">
-        {languages.map((language)=>{
-          return(
-            <li key={language}>
-              <button className="btn-clear nav-link">
-                {language}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selected = {selectedLanguage}
+          onUpdateLanguage = {this.updateLanguage}
+        />
+      </React.Fragment>
     );
   }
 }
