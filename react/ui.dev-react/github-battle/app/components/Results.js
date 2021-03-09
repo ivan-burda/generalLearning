64d1c3397b8cd999f5c4f06385a9adfc602bd88c
ivanburda1986 <burda.ivan@gmail.com>
2021-03-09 23:56:36 +0100

    finish battle results card display

--- react/ui.dev-react/github-battle/app/components/Results.js
+++ react/ui.dev-react/github-battle/app/components/Results.js
@@ -61,6 +61,9 @@ export default class Results extends React.Component{
               src={winner.profile.avatar_url}
               alt={`Avatar for ${winner.profile.login}`}
             />
+          <h4 className='center-text'>
+            Score: {winner.score.toLocaleString()}
+          </h4>
           <h2 className='center-text'>
             <a className='link' href={winner.profile.html_url}>
               {winner.profile.login}
@@ -71,13 +74,67 @@ export default class Results extends React.Component{
               <FaUser color='rgb(239,11,115)' size={22}/>
               {winner.profile.name}
             </li>
-            {winner.profile.location ? (<li></li>) : null}
+            {winner.profile.location ? (
+            <li>
+              <FaCompass color='rgb(144,115,255)' size={22}/>
+              {winner.profile.location}
+            </li>) : null}
+            {winner.profile.company ? (
+            <li>
+              <FaBriefcase color='#795548' size={22}/>
+              {winner.profile.company}
+            </li>) : null}
+            <li>
+              <FaUsers color='rgb(129,195,245)' size={22}/>
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64,183,95)' size={22}/>
+              {winner.profile.following.toLocaleString()} following
+            </li>
           </ul>
         </div>
         <div className='card bg-light'>
         <h4 className='header-lg center-text'>
             {winner.score===loser.score ? 'Tie' : 'Loser'}
           </h4>
+          <img
+              className='avatar'
+              src={loser.profile.avatar_url}
+              alt={`Avatar for ${loser.profile.login}`}
+            />
+           <h4 className='center-text'>
+            Score: {loser.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={loser.profile.html_url}>
+              {loser.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239,11,115)' size={22}/>
+              {loser.profile.name}
+            </li>
+            {loser.profile.location ? (
+            <li>
+              <FaCompass color='rgb(144,115,255)' size={22}/>
+              {loser.profile.location}
+            </li>) : null}
+            {loser.profile.company ? (
+            <li>
+              <FaBriefcase color='#795548' size={22}/>
+              {loser.profile.company}
+            </li>) : null}
+            <li>
+              <FaUsers color='rgb(129,195,245)' size={22}/>
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64,183,95)' size={22}/>
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
         </div>
       </div>
     )
