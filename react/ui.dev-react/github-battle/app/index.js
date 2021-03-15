d0e804dbf5cf601c2cd113be3a111d5c7cedc252
ivanburda1986 <burda.ivan@gmail.com>
2021-03-15 23:57:00 +0100

    implement routing

--- react/ui.dev-react/github-battle/app/index.js
+++ react/ui.dev-react/github-battle/app/index.js
@@ -5,6 +5,8 @@ import Popular from './components/Popular';
 import Battle from './components/Battle';
 import {ThemeProvider} from './contexts/theme';
 import Nav from './components/Nav';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 
 //Component
 //-State
@@ -27,14 +29,17 @@ class App extends React.Component{
   render(){
 
     return(
-      <ThemeProvider value = {this.state}>
-       <div className={this.state.theme}>
-        <div className="container">
-            <Nav/>
-            <Popular/>
-          </div>
-       </div>
-      </ThemeProvider>
+     <Router>
+        <ThemeProvider value = {this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+              <Nav/>
+              <Route exact path='/' component={Popular}/>
+              <Route path='/battle' component={Battle}/>
+            </div>
+        </div>
+        </ThemeProvider>
+     </Router>
     );
   }
 }
