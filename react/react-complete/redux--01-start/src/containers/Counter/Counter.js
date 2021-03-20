679bac56f0eb7058c02d49debaa76234c8df42a1
ivanburda1986 <burda.ivan@gmail.com>
2021-03-20 23:14:09 +0100

    larning redux - mapping state and actions

--- react/react-complete/redux--01-start/src/containers/Counter/Counter.js
+++ react/react-complete/redux--01-start/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,8 +29,10 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
+                {/* Instead of setting the value to the component state directly this.state.counter, I will link it to the redux props */}
+                <CounterOutput value={this.props.ctr} />  
+                {/* <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} /> */}
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
@@ -38,4 +41,20 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+//Instructions on how the component's state gets mapped to redux store
+const mapStateToProps = (state) =>{
+    return {
+        ctr: state.counter
+    }
+};
+
+//Instructions on how the component's actions get mapped to the redux store
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onIncrementCounter: () => dispatch ({type: 'INCREMENT'})    //As this is a one-liner I don't have to use curly braces when returning from the arrow function
+    }
+};
+
+//The connect can take two arguments: mapping of the state and mapping of the actions
+//If there was nothing to pass a state, I should just pass in: null
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- react/react-complete/redux--01-start/src/index.js
+++ react/react-complete/redux--01-start/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+//I have to pass to the Provider a store. In this case the store I pass in is the store that I have created above
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
