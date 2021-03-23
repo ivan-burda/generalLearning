92c39e4a7f3fb805595f6327c70b4178dd188cac
ivanburda1986 <burda.ivan@gmail.com>
2021-03-23 23:17:28 +0100

    started with the assignemnt

--- react/react-complete/redux--assignment-2-problem/src/index.js
+++ react/react-complete/redux--assignment-2-problem/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Reducers
+
+//Combine reducers
+
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
