0b75041b59fc78d0651ae23e95ce22292f111a3d
ivanburda1986 <burda.ivan@gmail.com>
2021-03-25 22:08:35 +0100

    implement own name and age for the exercise

--- react/react-complete/redux--assignment-2-problem/src/containers/Persons.js
+++ react/react-complete/redux--assignment-2-problem/src/containers/Persons.js
@@ -32,7 +32,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onPersonAdd: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onPersonAdd: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name:name, age: age}}),
         onPersonDelete: (personId) => dispatch({type: actionTypes.DELETE_PERSON, personId: personId}),
     }
 };
--- react/react-complete/redux--assignment-2-problem/src/store/reducers/personsReducer.js
+++ react/react-complete/redux--assignment-2-problem/src/store/reducers/personsReducer.js
@@ -5,14 +5,15 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  const newPerson = {
-    id: Math.random(), // not really unique but good enough here!
-    name: 'Max',
-    age: Math.floor( Math.random() * 40 )
-  }
 
   switch(action.type){
-    case actionTypes.ADD_PERSON: return{
+    case actionTypes.ADD_PERSON: 
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: action.personData.name,
+        age: action.personData.age
+      }
+      return{
       ...state,
       persons: state.persons.concat(newPerson)
     };
