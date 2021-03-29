77997a0f3c8dd3c1012ecd4dd16dfe0944c8a3ce
ivanburda1986 <burda.ivan@gmail.com>
2021-03-29 22:34:30 +0200

    redux devtools

--- react/react-complete/redux--01-start/src/index.js
+++ react/react-complete/redux--01-start/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
@@ -14,7 +14,22 @@ const rootReducer = combineReducers({
   res: resultReducer, 
 });
 
-const store = createStore(rootReducer);
+
+const logger = (store) => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  }
+};
+
+//Connects the Redux devtools to the Redux store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ 
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 //I have to pass to the Provider a store. In this case the store I pass in is the store that I have created above
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
