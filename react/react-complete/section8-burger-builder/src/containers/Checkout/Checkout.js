5cfde8b3c364321d49874609223af50ecca01aff
ivanburda1986 <burda.ivan@gmail.com>
2021-04-02 00:15:35 +0200

    redux

--- react/react-complete/section8-burger-builder/src/containers/Checkout/Checkout.js
+++ react/react-complete/section8-burger-builder/src/containers/Checkout/Checkout.js
@@ -8,10 +8,6 @@ import * as actions from '../../store/actions/index';
 
 class Checkout extends Component{
 
-  componentDidMount () {
-
-  }
-
   checkoutCancelledHandler = () =>{
     this.props.history.goBack();
   }
@@ -23,9 +19,11 @@ class Checkout extends Component{
   render(){
     let summary = <Redirect to="/"/>
     if(this.props.ings){
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
       summary = (
 
         <div>
+          {purchasedRedirect}
            <CheckoutSummary 
                 ingredients={this.props.ings}
                 checkoutCancelled={this.checkoutCancelledHandler}
@@ -45,14 +43,7 @@ class Checkout extends Component{
 const mapStateToProps = (state) =>{
   return{
     ings: state.burgerBuilder.ingredients,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onInitPurchase: () => {
-
-    }
+    purchased: state.order.purchased
   }
 }
 
