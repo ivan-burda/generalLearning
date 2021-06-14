6362ed267c08bb561c9e1624a118ad9b6071443d
ivanburda1986 <burda.ivan@gmail.com>
2021-06-14 23:38:58 +0200

    add poll

--- react/ui.dev-redux-2ndTime/project-polls/src/components/App.js
+++ react/ui.dev-redux-2ndTime/project-polls/src/components/App.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { handleInitialData } from "../actions/shared";
 import Leaderboard from "../components/Leaderboard";
 import Dashboard from "../components/Dashboard";
+import Nav from "../components/Nav";
+import AddPoll from "../components/AddPoll";
+import Poll from "../components/Poll";
 
 export default function App() {
   const dispatch = useDispatch();
@@ -17,7 +20,25 @@ export default function App() {
   console.log("Store", store);
   return (
     <Router>
-      <div className="container">{loading === true ? null : <Dashboard />}</div>
+      <div className="container">
+        <Nav />
+        {loading === true ? null : (
+          <div>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/polls/:id">
+              <Poll />
+            </Route>
+            <Route path="/add">
+              <AddPoll />
+            </Route>
+          </div>
+        )}
+      </div>
     </Router>
   );
 }
---
+++ react/ui.dev-redux-2ndTime/project-polls/src/components/Nav.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <NavLink to="/" exact activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/leaderboard" exact activeClassName="active">
+        Leaderboard
+      </NavLink>
+      <NavLink to="/add" exact activeClassName="active">
+        Add Poll
+      </NavLink>
+    </nav>
+  );
+}
