20efa4e43c5cc7df4032ffc7b938f0b200df1496
ivanburda1986 <burda.ivan@gmail.com>
2021-08-21 22:51:13 +0200

    finished the basketball project

--- react/ui.dev-reactRouter5/basketbalLeagueProject-guided/src/components/App.js
+++ react/ui.dev-reactRouter5/basketbalLeagueProject-guided/src/components/App.js
@@ -5,6 +5,8 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Players from "./Players";
 import Teams from "./Teams";
+import TeamPage from "./TeamPage";
+import Articles from "./Articles";
 
 export default function App() {
   return (
@@ -20,6 +22,12 @@ export default function App() {
           </Route>
           <Route path="/teams">
             <Teams />
+          </Route>
+          <Route path="/:teamId" exact>
+            <TeamPage />
+          </Route>
+          <Route path="/:teamId/articles">
+            <Articles />
           </Route>
           <Route path="*">
             <h1 className="text-center">404 Not Found</h1>
