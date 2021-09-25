2245bbd9d287996f8dc8edf04b6fecbb6bb2e87f
ivanburda1986 <burda.ivan@gmail.com>
2021-09-25 23:44:45 +0200

    ts

--- udemy-max-typescript/course-starting-project/app.ts
+++ udemy-max-typescript/course-starting-project/app.ts
@@ -13,21 +13,34 @@
 
 // string[] = an array of strings
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //this specifies a tuple: an array with a specific length and item types. Controls when we want to assign or re-asign items in the array (tuple); however by using .push() we would still be able to insert more elements - typescript does not catch this
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]; //number[]
+//   role: [number, string]; //this specifies a tuple: an array with a specific length and item types. Controls when we want to assign or re-asign items in the array (tuple); however by using .push() we would still be able to insert more elements - typescript does not catch this
+// } = {
+//   name: "Maxmilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Maxmilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: 4,
 };
 
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase()); //Here typescript knows hobby is definitely a string so it can even suggest available methods for a string which makes writing code easier
-  console.log(hobby.map()); //here typescript correctly complains that map() is not a method of a string
+  //console.log(hobby.map()); //here typescript correctly complains that map() is not a method of a string
 }
