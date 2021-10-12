3471ef8fc3fa8b22fdb7ad4241a3550032a1ebaf
ivanburda1986 <burda.ivan@gmail.com>
2021-10-13 00:24:00 +0200

    practice

---
+++ ui.dev-typescript/17-discriminating-unions.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface MovingThing {
+  speed: number;
+}
+
+// Add the necessary properties to allow for discriminating unions
+interface Car extends MovingThing {
+  type: "car";
+  wheels: number;
+}
+interface Boat extends MovingThing {
+  type: "boat";
+  drag: number;
+}
+interface Plane extends MovingThing {
+  type: "plane";
+  drag: number;
+  engines: number;
+}
+interface Train extends MovingThing {
+  type: "train";
+  cars: number;
+  wheels: number;
+}
+
+type Vehicle = Car | Boat | Plane | Train;
+
+// Without changing the parameter type, use discriminating unions
+// to fix the type errors
+function speed(vehicle: Vehicle) {
+  console.log(vehicle.speed);
+}
+function wheelCount(vehicle: Vehicle) {
+  if (vehicle.type === "car" || vehicle.type === "train") {
+    console.log(vehicle.wheels);
+  }
+}
+function dragAmount(vehicle: Vehicle) {
+  if (vehicle.type === "boat" || vehicle.type === "plane") {
+    // or: if("drag" in vehicle){}
+    console.log(vehicle.drag);
+  }
+}
+function numberOfCars(vehicle: Vehicle) {
+  if (vehicle.type === "train") {
+    console.log(vehicle.cars);
+  }
+}
