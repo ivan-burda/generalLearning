b5341fcc2bcc0ed74f2f671d763536ecacf5a541
ivanburda1986 <burda.ivan@gmail.com>
2021-11-02 21:58:21 +0100

    webpack

--- udemy-max-typescript/project-drag-and-drop/webpack.config.js
+++ udemy-max-typescript/project-drag-and-drop/webpack.config.js
@@ -1,9 +1,39 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./scr/app.ts",
+  mode: "development",
+  entry: "./src/app.ts",
+  performance: {
+    hints: false,
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    // clear: true
+    publicPath: "/dist/",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./"),
+    },
+    compress: true,
+    port: 3000,
+  },
+  resolve: {
+    extensions: [
+      // it is an array
+      ".ts",
+      ".js",
+    ],
   },
 };
