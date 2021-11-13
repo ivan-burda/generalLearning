c79911b7c4d12aa1aa2805bc9928445c4c297366
ivanburda1986 <burda.ivan@gmail.com>
2021-11-13 19:03:10 +0100

    finished the todo project

--- udemy-max-typescript/project-react-and-typescript/src/components/NewTodo.tsx
+++ udemy-max-typescript/project-react-and-typescript/src/components/NewTodo.tsx
@@ -1,17 +1,22 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
-export const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+export const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
-    console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
