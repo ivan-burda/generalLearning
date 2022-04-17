897f6a8800ff1f44f3433240f2bcb5196e38a54e
Ivan Burda <ivan.burda@erstegroup.com>
2022-04-17 23:58:34 +0200

    factory pattern

--- udemy-algorithms/power.ts
+++ udemy-algorithms/power.ts
@@ -1,7 +1,5 @@
 function power(base, exponent){
-    if(exponent === 0){
-        return 1;
-    }
+    if(exponent === 0) return 1;
     let result = 1;
     for(let i = 1; i<=exponent; i++){
         result = result * base;
