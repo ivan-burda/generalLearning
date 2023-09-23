abb75186ce05c6a45bf510921127a2d446e86094
ivanburda1986 <burda.ivan@gmail.com>
2023-09-23 21:18:45 +0200

    button logics

--- udemy-rnative/expense-tracker/screens/ManageExpense.js
+++ udemy-rnative/expense-tracker/screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 import { IconButton } from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import { Button } from "../components/UI/Button";
 
 export const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -16,10 +17,26 @@ export const ManageExpense = ({ route, navigation }) => {
   if (isEditing) {
   }
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+  function cancelHandler() {
+    navigation.goBack();
+  }
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -39,6 +56,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
