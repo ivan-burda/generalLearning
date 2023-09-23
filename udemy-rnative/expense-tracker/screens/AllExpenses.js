7e055ee4085e891a9163bcb92e9a926205354260
ivanburda1986 <burda.ivan@gmail.com>
2023-09-23 21:01:30 +0200

    adding screens

--- udemy-rnative/expense-tracker/screens/AllExpenses.js
+++ udemy-rnative/expense-tracker/screens/AllExpenses.js
@@ -1,5 +1,6 @@
 import { Text } from "react-native";
+import { ExpensesOutput } from "../components/ExpensesOutput/ExpensesOutput";
 
 export const AllExpenses = () => {
-  return <Text>All Expenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod="Total" />;
 };
--- udemy-rnative/expense-tracker/screens/RecentExpenses.js
+++ udemy-rnative/expense-tracker/screens/RecentExpenses.js
@@ -1,5 +1,6 @@
 import { Text } from "react-native";
+import { ExpensesOutput } from "../components/ExpensesOutput/ExpensesOutput";
 
 export const RecentExpenses = () => {
-  return <Text>Recent Expenses Screen</Text>;
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
 };
