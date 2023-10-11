e37c6511f831f99033ee23db2c13c61b7f2ccd4e
ivanburda1986 <burda.ivan@gmail.com>
2023-10-11 23:30:33 +0200

    error handling

--- udemy-rnative/authentication/my-app/screens/LoginScreen.js
+++ udemy-rnative/authentication/my-app/screens/LoginScreen.js
@@ -2,13 +2,21 @@ import { loginUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import AuthContent from "../components/Auth/AuthContent";
 import { useState } from "react";
+import { Alert } from "react-native";
 
 export const LoginScreen = () => {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await loginUser(email, password);
+    try {
+      await loginUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Cannot login. Please check credentials",
+      );
+    }
     setIsAuthenticating(false);
   }
 
--- udemy-rnative/authentication/my-app/screens/SignupScreen.js
+++ udemy-rnative/authentication/my-app/screens/SignupScreen.js
@@ -1,14 +1,23 @@
 import AuthContent from "../components/Auth/AuthContent";
-import { createUser } from "../util/auth";
+import { createUser, loginUser } from "../util/auth";
 import { useState } from "react";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 export const SignupScreen = () => {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "User creation failed",
+        "Cannot create a new user. Please check entered data and try again",
+      );
+    }
+
     setIsAuthenticating(false);
   }
 
