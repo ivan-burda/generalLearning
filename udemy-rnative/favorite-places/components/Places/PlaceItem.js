2c3db8a7bda7e232a43a80848e4c442cc258c1e1
ivanburda1986 <burda.ivan@gmail.com>
2023-10-15 18:06:35 +0200

    add header btn

---
+++ udemy-rnative/favorite-places/components/Places/PlaceItem.js
@@ -0,0 +1,15 @@
+import { Pressable, View, StyleSheet } from "react-native";
+
+export const PlaceItem = ({ place, onSelect }) => {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{ uri: place.imageUri }} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ udemy-rnative/favorite-places/screens/AllPlaces.js
@@ -0,0 +1,5 @@
+import { PlacesList } from "../components/Places/PlacesList";
+
+export const AllPlaces = () => {
+  return <PlacesList />;
+};
