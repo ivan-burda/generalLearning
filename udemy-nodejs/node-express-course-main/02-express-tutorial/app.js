4de06116ee7e262cb309303ad95e62e4c15bdf0f
ivan-burda <burda.ivan@protonmail.com>
2025-01-20 11:55:40 +0100

    njs

--- udemy-nodejs/node-express-course-main/02-express-tutorial/app.js
+++ udemy-nodejs/node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000!");
+});
