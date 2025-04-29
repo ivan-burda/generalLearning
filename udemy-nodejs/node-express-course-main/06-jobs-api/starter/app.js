9e217b3ae56a7027c48a8a25d05d4f8c678433e9
ivan-burda <burda.ivan@protonmail.com>
2025-04-29 14:27:22 +0200

    auth

--- udemy-nodejs/node-express-course-main/06-jobs-api/starter/app.js
+++ udemy-nodejs/node-express-course-main/06-jobs-api/starter/app.js
@@ -13,7 +13,7 @@ const app = express();
 //connectDB
 const connectDB = require("./db/connect");
 
-const authenticateUser = require("./middleware/authentication");
+const authenticateUser = require('./middleware/authentication');
 
 // routers
 const authRouter = require("./routes/auth");
@@ -36,7 +36,7 @@ app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
