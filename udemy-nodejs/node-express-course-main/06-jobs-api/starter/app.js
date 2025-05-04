14c8b559a23f52d90f5cc261b5f481e203ae5159
ivan-burda <burda.ivan@protonmail.com>
2025-05-04 22:43:48 +0200

    add yaml docs

--- udemy-nodejs/node-express-course-main/06-jobs-api/starter/app.js
+++ udemy-nodejs/node-express-course-main/06-jobs-api/starter/app.js
@@ -10,6 +10,11 @@ const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //connectDB
 const connectDB = require("./db/connect");
 
@@ -33,6 +38,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// })
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
