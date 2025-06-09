70e831db15aee8226a3310a2b4cfa27e305ff6ae
ivan-burda <burda.ivan@protonmail.com>
2025-06-09 16:01:27 +0200

    imageupload

---
+++ udemy-nodejs/node-express-course-main/07-file-upload/starter/routes/productRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const {createProduct, getAllProducts} = require('../controllers/productController');
+const {uploadProductImageCloudinary} = require('../controllers/uploadsController');
+
+router.route('/').post(createProduct).get(getAllProducts);
+router.route('/uploads').post(uploadProductImageCloudinary);
+
+module.exports = router;
